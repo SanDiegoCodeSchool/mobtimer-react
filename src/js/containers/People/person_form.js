3ca4d3c2f92555ad230d0e18b90005c698e85c83
Ztruk11 <danielmkurtz@gmail.com>
2019-02-04 17:42:07 -0800

    working names

--- src/js/containers/People/person_form.js
+++ src/js/containers/People/person_form.js
@@ -1,53 +1,62 @@
 import React from 'react';
-import axios from "axios";
+import axios from 'axios';
 
-import {updatePeople, getStudents, selectStudent} from './peopleActions';
+import { updatePeople, getStudents, selectStudent } from './peopleActions';
 
 class PersonForm extends React.Component {
-  constructor(props) {
-    super(props);
-    this.handleOptionChange = this.handleOptionChange.bind(this)
-    this.handleAdd = this.handleAdd.bind(this)
-    this.state = {
-      name: '',
-      id: null
-    };
-  }
+    constructor(props) {
+        super(props);
+        this.handleOptionChange = this.handleOptionChange.bind(this);
+        this.handleAdd = this.handleAdd.bind(this);
+        this.state = {
+            name: '',
+            id: ''
+        };
+    }
 
-  componentWillMount() {
-    const { dispatch } = this.props;
+    componentWillMount() {
+        const { dispatch } = this.props;
 
-    dispatch(getStudents());
-  }
+        dispatch(getStudents());
+    }
 
-  handleOptionChange(event) {
-    const { dispatch } = this.props;
-    console.log('event.target', event.target);
-    this.setState({id: event.target.value })
-    // dispatch(selectStudent(event.target.value));
-  }
+    handleOptionChange(event) {
+        const { dispatch } = this.props;
+        console.log('event.target', event.target);
+        this.setState({ id: event.target.value });
+        // dispatch(selectStudent(event.target.value));
+    }
 
-  handleAdd(event) {
-    const { dispatch } = this.props;
-    event.preventDefault();
-    dispatch(updatePeople(this.state.id));
-  }
+    handleAdd(event) {
+        const { dispatch } = this.props;
+        event.preventDefault();
+        dispatch(updatePeople(this.state.id));
+    }
 
-  render() {
-    const {studentList} = this.props;
-    console.log(this.props.studentList);
-    return (
-      <form onSubmit={this.handleAdd} className="people-form">
-        <select onChange={this.handleOptionChange} value={this.state.id}>
-              <option>Please Select Your Name!</option>
-              {studentList.map((eachStudent) => <option value={eachStudent.id} key={eachStudent.id}>{eachStudent.fName} {eachStudent.lName}</option>)}
-        </select>
-        <button type="submit">Add</button>
-      </form>
-    );
-  }
-};
-
-
+    render() {
+        const { studentList } = this.props;
+        return (
+            <form onSubmit={this.handleAdd} className="people-form">
+                <select
+                    onChange={this.handleOptionChange}
+                    value={this.state.id}
+                >
+                    <option value="" disabled selected>
+                        Please Select Your Name!
+                    </option>
+                    {studentList.map(eachStudent => (
+                        <option
+                            value={eachStudent.fName + ' ' + eachStudent.lName}
+                            key={eachStudent.id}
+                        >
+                            {eachStudent.fName} {eachStudent.lName}
+                        </option>
+                    ))}
+                </select>
+                <button type="submit">Add</button>
+            </form>
+        );
+    }
+}
 
 export default PersonForm;
